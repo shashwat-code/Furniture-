a352d6f6051b13042e5cd82f2feec4e3d0233be0
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-01-31 11:52:35 +0530

    Initial Commit

---
+++ Furniture !/Furniture__App.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture__App.swift
+//  Furniture !
+//
+//  Created by Shashwat . on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Furniture__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
